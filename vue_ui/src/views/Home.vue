fca1ed9efa8631546d1a863050fb7aa2229f725b
saiki takanari <saikitakanari@saikinoMacBook-Air.local>
2020-10-21 07:31:51 +0900

    remove routes

--- vue_ui/src/views/Home.vue
+++ vue_ui/src/views/Home.vue
@@ -2,12 +2,6 @@
 <div>
     <div id="nav">
         <p>
-            <router-link to="/">Home</router-link>
-        </p>
-        <p>
-            <router-link to="/about">About</router-link>
-        </p>
-        <p>
             <router-link to="/todo_board">TodoBoard</router-link>
         </p>
     </div>
